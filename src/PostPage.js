98c488f076421106080367877e8a750fec84d1fe
Lolu <66273292+thisdotLolu@users.noreply.github.com>
2022-03-25 02:28:09 +0100

    Add files via upload

---
+++ src/PostPage.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+
+
+const PostPage = ({posts, handleDelete}) => {
+  const {id}=useParams()
+  const post=posts.find(post=>(post.id).toString()===id)
+
+  return (
+    <main className='PostPage'>
+        <article className='post'>
+            {post &&
+            <>
+            <h2>{post.title}</h2>
+            <p className='postDate'>{post.datetime}</p>
+            <p className='postDate'>{post.body}</p>
+            <button onClick={()=>{handleDelete(post.id)}}>Delete Post</button>
+            </>
+            }
+            {!post && <>
+            <h2>Post Not Found</h2>
+            <p>Well, that's disappointing</p>
+            <p>
+                <Link to='/'>Go back to HomePage</Link>
+            </p>
+            
+            </>
+
+            }
+        </article>
+    </main>
+  )
+}
+
+export default PostPage
